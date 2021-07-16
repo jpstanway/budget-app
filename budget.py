4838e9aa0ef5c2a991deed2e6c65ae302907237d
Jordan S <jpstanway@gmail.com>
2021-07-15 21:35:51 -0700

    create basic func for deposit and get balance

--- budget.py
+++ budget.py
@@ -1,4 +1,29 @@
 class Category:
+    ledger = []
+
+    def __init__(self, category):
+        self.category = category
+
+    def deposit(self, amt, desc=''):
+        self.ledger.append({"amount": amt, "description": desc})
+
+    def withdraw(self, amt, desc=''):
+        return True
+
+    def get_balance(self):
+        balance = 0
+
+        for transaction in self.ledger:
+            balance += int(transaction['amount'])
+
+        return balance
+
+    def transfer(self, amt, category):
+        return True
+
+    def check_funds(self, amt):
+        return True
 
 
 def create_spend_chart(categories):
+    return categories
